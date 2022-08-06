301edbe0e6a4180169a64ba1817a378657ef9808
M.Said Gürbüz <42msg42@gmail.com>
2022-08-06 15:25:32 +0300

    Open api/Semantic-ui added,MySql Connected

--- src/main/java/com/trackingsys/stocktrackingsystem/StockTrackingSystemApplication.java
+++ src/main/java/com/trackingsys/stocktrackingsystem/StockTrackingSystemApplication.java
@@ -1,8 +1,12 @@
 package com.trackingsys.stocktrackingsystem;
 
-
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StockTrackingSystemApplication {
@@ -11,6 +15,15 @@ public class StockTrackingSystemApplication {
 		SpringApplication.run(StockTrackingSystemApplication.class, args);
 	}
 
-
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application-description}") String description,
+								 @Value("${application-version}") String version){
+		return new OpenAPI()
+				.info(new Info()
+						.title("Msg OpenAPI Test")
+						.version(version)
+						.description(description)
+						.license(new License().name("Msg OpenAPI Test Licence")));
+	}
 
 }
