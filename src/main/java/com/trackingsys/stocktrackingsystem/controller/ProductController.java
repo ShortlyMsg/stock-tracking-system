87c1bbda6db3d42b307af2ba3b72cf754b0992e9
M.Said Gürbüz <42msg42@gmail.com>
2022-08-07 07:23:58 +0300

    Slf4j/log.info added to Service and Controller

--- src/main/java/com/trackingsys/stocktrackingsystem/controller/ProductController.java
+++ src/main/java/com/trackingsys/stocktrackingsystem/controller/ProductController.java
@@ -2,11 +2,13 @@ package com.trackingsys.stocktrackingsystem.controller;
 
 import com.trackingsys.stocktrackingsystem.entity.Product;
 import com.trackingsys.stocktrackingsystem.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -19,16 +21,19 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<Product> addProduct(@RequestBody Product product){
+        log.info("Inside addProduct method of UserController");
         return ResponseEntity.ok(productService.addProduct(product));
     }
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts(){
+        log.info("Inside getAllProducts method of UserController");
         return ResponseEntity.ok(productService.getAllProducts());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable("id") Long productId){
+        log.info("Inside getProductById method of UserController");
         return ResponseEntity.ok(productService.getProductById(productId));
     }
 }
--- src/main/java/com/trackingsys/stocktrackingsystem/service/ProductService.java
+++ src/main/java/com/trackingsys/stocktrackingsystem/service/ProductService.java
@@ -2,10 +2,12 @@ package com.trackingsys.stocktrackingsystem.service;
 
 import com.trackingsys.stocktrackingsystem.entity.Product;
 import com.trackingsys.stocktrackingsystem.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class ProductService {
 
@@ -16,14 +18,17 @@ public class ProductService {
     }
 
     public Product addProduct(Product product) {
+        log.info("Inside addProduct method of ProductService");
         return productRepository.save(product);
     }
 
     public List<Product> getAllProducts() {
+        log.info("Inside getAllProducts method of ProductService");
         return productRepository.findAll();
     }
 
     public Product getProductById(Long productId) {
+        log.info("Inside getProductById method of ProductService");
         return productRepository.findById(productId)
                 .orElse(null);
     }
