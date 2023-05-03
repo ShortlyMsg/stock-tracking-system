2131d397a491019fae46eec887eb9dd24f475999
M.Said Gürbüz <42msg42@gmail.com>
2023-05-03 09:48:11 +0300

    Config file & SecurityConfig added(Without WebSecurityConfigurerAdapter). DB changed.

---
+++ src/main/java/com/trackingsys/stocktrackingsystem/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.trackingsys.stocktrackingsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager configAuthentication(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
