e709731a3c378eb7843b2bc529d2da7297a17753
M.Said Gürbüz <42msg42@gmail.com>
2023-05-22 07:27:24 +0300

    HttpBasic changed to OAuth2(dependency added). Github and Google OAuth2 integrated

--- src/main/java/com/trackingsys/stocktrackingsystem/config/SecurityConfig.java
+++ src/main/java/com/trackingsys/stocktrackingsystem/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.trackingsys.stocktrackingsystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -32,17 +33,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        return http.httpBasic()
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html","/error").permitAll()
-                //.requestMatchers("/login").permitAll()
-                .requestMatchers("/orders").authenticated()
-                .requestMatchers(HttpMethod.POST,"/users/**").hasRole("USER")
-                .requestMatchers(HttpMethod.GET,"/users/**").hasRole("ADMIN")
-                .and()
-                .csrf().disable()
-                .formLogin().disable()
+        return http
+                .authorizeHttpRequests(auth ->{
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults())
                 .build();
     }
 
